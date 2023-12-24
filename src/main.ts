1895f8fee23d0ef5f4ecf87b917984af8e38d1ac
basileus <timberspirit@gmail.com>
2023-12-24 22:01:46 +0200

    feat(task-8): wrapped nest app into the lambda handler

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { configure as serverlessExpress } from '@vendia/serverless-express';
 
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
+import type { Callback, Context, Handler } from 'aws-lambda';
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4040;
 
-async function bootstrap() {
+async function bootstrap(event: any): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -14,8 +16,23 @@ async function bootstrap() {
   });
   app.use(helmet());
 
-  await app.listen(port);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+
+
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
+
+let server: Handler | undefined;
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  console.log('EVENT:', JSON.stringify(event));
+
+  server = server ?? (await bootstrap(event));
+  return server(event, context, callback);
+};
